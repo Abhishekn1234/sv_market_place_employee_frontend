313533659eea4af36e8f7a605c9c20341c2741c2
Abhishek N <abhishekpes123@gmail.com>
2026-06-27 12:36:28 +0530

    `Removed unused import and type from WalletRepoImpl`

--- src/pages/Wallet/data/repositories/WalletRepoImpl.ts
+++ src/pages/Wallet/data/repositories/WalletRepoImpl.ts
@@ -1,5 +1,5 @@
 import api from "@/api/api";
-import type { WalletSummary, WalletTransactionsResponse, WalletTransactionsParams } from "../../domain/entities/wallet";
+import type { WalletSummary } from "../../domain/entities/wallet";
 import type { WalletRepo } from "../../domain/repositories/WalletRepo";
 
 export class WalletRepoImpl implements WalletRepo {
